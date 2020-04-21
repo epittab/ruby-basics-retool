b120630eaccc0eedcfd56881177b7fbd8cfba0f3
epittab <ep7m@me.com>
2020-04-21 14:13:31 -0400

    first commit; completed 1 and 2

--- newsletter.rb
+++ newsletter.rb
@@ -30,8 +30,8 @@ def calculate_recipients
   # write a method that will return an array of only the subscribers who haven't unsubscribed
 end
 
-def first_n_articles(number_of_articles
-  ARTICLES.first(number_of_articles)
+def first_n_articles(number_of_articles)
+  ARTICLES.first(number_of_articles.to_i)
 end
 
 def print_recipients
@@ -52,7 +52,8 @@ def print_many_articles(articles)
 end
 
 def format_campus_location(campus)
-  "Flatiron #{campus["name"]}"
+  
+  "Flatiron #{campus[:name]}"
 end
 
 def format_subject
@@ -78,7 +79,7 @@ def print_newsletter(number)
   print_many_articles(articles)
   puts format_footer(CAMPUS)
 
-  end
+
 end
 
 def run
